a6f644f874f4948dd2297fcac7a3fab4dc7bc4bd
19ewalker <44076192+19ewalker@users.noreply.github.com>
2022-04-24 13:43:00 -0400

    Modified style and function of prev/next buttons in LVL 1

--- static/learn_letter.js
+++ static/learn_letter.js
@@ -29,6 +29,7 @@ function show_info(info){
 
     $("#form").submit(function(event){
         $("#input-feedback").empty()
+        $("#change-state").empty()
         event.preventDefault()
         answerVal = $("#hangul-question").val()
         if($.trim(answerVal) == stats["pronunciation"]){
@@ -37,25 +38,28 @@ function show_info(info){
         else{
             $("#input-feedback").append("Incorrect. Try again.")
         }
+        let isCorrect= document.getElementById("input-feedback").innerHTML
+        console.log(isCorrect)
+        $("#change-state").append("<div class = 'd-flex justify-content-between prev-next'>")
+
+        let prev_id = stats["id"] - 1
+        let curr_id = stats["id"]
+        let next_id = stats["id"] + 1
+
+
+        if(stats["end"]=="1" && isCorrect=="Correct!"){
+            $("#change-state").append("<a class = 'p-3 prev next-button btn btn-light' href='"+ prev_id +"'>← PREVIOUS</a>")
+            $("#change-state").append("<a class = 'p-3 prev next-button btn btn-light' href = '../../quiz/class1/letter'>NEXT →</a></div>")
+        }
+        else if(prev_id == 0 && isCorrect=="Correct!"){
+            $("#change-state").append("<a class = 'p-3 prev next-button btn btn-light' href='#'>← PREVIOUS</a>")
+            $("#change-state").append("<a class = 'p-3 prev next-button btn btn-light' href = '" + next_id+"'>NEXT →</a></div>")
+        }
+        else if(isCorrect=="Correct!"){
+            $("#change-state").append("<a class = 'p-3 prev next-button btn btn-light' href='"+prev_id +"'>← PREVIOUS</a>")
+            $("#change-state").append("<a class = 'p-3 prev next-button btn btn-light' href = '"+ next_id+"'>NEXT →</a></div>")
+        }
     });
-    $("#change-state").append("<div class = 'd-flex justify-content-between prev-next'>")
-
-    let prev_id = stats["id"] - 1
-    let curr_id = stats["id"]
-    let next_id = stats["id"] + 1
-
-    if(stats["end"]=="1"){
-        $("#change-state").append("<a class = 'p-3 prev next-button' href='127.0.0.1:5000/learn/letter"+ prev_id +"'>← PREVIOUS</a>")
-        $("#change-state").append("<a class = 'p-3 prev next-button' href = '../../quiz/class1/letter/1'>NEXT →</a></div>")
-    }
-    else if(prev_id == 0){
-        $("#change-state").append("<a class = 'p-3 prev next-button' href='#'>← PREVIOUS</a>")
-        $("#change-state").append("<a class = 'p-3 prev next-button' href = '" + next_id+"'>NEXT →</a></div>")
-    }
-    else{
-        $("#change-state").append("<a class = 'p-3 prev next-button' href='"+prev_id +"'>← PREVIOUS</a>")
-        $("#change-state").append("<a class = 'p-3 prev next-button' href = '"+ next_id+"'>NEXT →</a></div>")
-    }
 }
 function find_letter(info){
     $.ajax({
