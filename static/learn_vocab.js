4645483bc20a6759d7b6a77a98229fcb066efde8
Jianyu Gao <jzg2108@columbia.edu>
2022-04-25 15:23:53 -0400

    A whole lot of UI fixing across modules.

--- static/learn_vocab.js
+++ static/learn_vocab.js
@@ -5,7 +5,7 @@ function display_info(info){
     $("#hangul-character").empty()
     $("#hangul-image").empty()
     $("#hangul-desc").empty()
-    $("#input-feedback").empty()
+    $("#check-work").empty()
     console.log(info)
     find_word(info)
 }
@@ -20,25 +20,25 @@ function play() {
 
 function show_info(info){
     $("#hangul-character").append(stats["hangul"])
-    $("#image").append("<img src ='"+ stats["image"]+"' width = '200'></img>")
+    $("#hangul-image-div").append("<img id='hangul-image' src ='" + stats["image"] + "'/>")
     $("#hangul-audio").on("click", function() {
         play()
     })
     $("#hangul-desc").append("<p> Pronunciation: '" + stats["pronunciation"] + "'</p>")
     $("#hangul-desc").append("<p> Definition: " + stats["definition"] + "</p>")
-    $(".question").append("<label for='hangul-question-1'>How do you pronounce <b>"+stats["hangul"]+"</b>?</label>")
-    $(".question").append("<label for='hangul-question-2'>What does <b>"+stats["hangul"]+"</b> mean?</label>")
+    $("#question-1").append("<label for='hangul-question-1'>How do you pronounce <b>"+stats["hangul"]+"</b>?</label>")
+    $("#question-2").append("<label for='hangul-question-2'>What does <b>"+stats["hangul"]+"</b> mean?</label>")
 
 
     $("#form").submit(function(event){
-        $("#input-feedback").empty()
+        $("#check-work").empty()
         event.preventDefault()
         answerVal = $("#hangul-question-1").val()
         if($.trim(answerVal) == stats["pronunciation"]){
-            $("#input-feedback").append("Correct!")
+            $("#check-work").append("Correct!")
         }
         else{
-            $("#input-feedback").append("Incorrect. Try again.")
+            $("#check-work").append("Incorrect. Try again.")
         }
     });
     $("#change-state").append("<div class = 'd-flex justify-content-between prev-next'>")
@@ -48,16 +48,16 @@ function show_info(info){
     let next_id = stats["id"] + 1
 
     if(stats["end"]=="1"){
-        $("#change-state").append("<a class = 'p-3 prev next-button' href='127.0.0.1:5000/learn/letter"+ prev_id +"'>← PREVIOUS</a>")
-        $("#change-state").append("<a class = 'p-3 prev next-button' href = '../../quiz/class1/letter/1'>NEXT →</a></div>")
+        $("#change-state").append("<a class = 'p-3 mr-auto btn prev-next-button' href='127.0.0.1:5000/learn/letter"+ prev_id +"'>← PREVIOUS</a>")
+        $("#change-state").append("<a class = 'p-3 btn prev-next-button' href = '../../quiz/class1/letter/1'>NEXT →</a></div>")
     }
     else if(prev_id == 0){
-        $("#change-state").append("<a class = 'p-3 prev next-button' href='#'>← PREVIOUS</a>")
-        $("#change-state").append("<a class = 'p-3 prev next-button' href = '" + next_id+"'>NEXT →</a></div>")
+        $("#change-state").append("<a class = 'p-3 mr-auto btn prev-next-button' href='#'>← PREVIOUS</a>")
+        $("#change-state").append("<a class = 'p-3 btn prev-next-button' href = '" + next_id+"'>NEXT →</a></div>")
     }
     else{
-        $("#change-state").append("<a class = 'p-3 prev next-button' href='"+prev_id +"'>← PREVIOUS</a>")
-        $("#change-state").append("<a class = 'p-3 prev next-button' href = '"+ next_id+"'>NEXT →</a></div>")
+        $("#change-state").append("<a class = 'p-3 mr-auto btn prev-next-button' href='"+prev_id +"'>← PREVIOUS</a>")
+        $("#change-state").append("<a class = 'p-3 btn prev-next-button' href = '"+ next_id+"'>NEXT →</a></div>")
     }
 }
 function find_word(info){
